bad3be36b0505dada85e6b2fe5184ce3a3930cb6
CassieAin <roksolana.d22@gmail.com>
2017-11-08 14:06:35 +0200

    Conversions refactoring

--- src/task1/Conversions.java
+++ src/task1/Conversions.java
@@ -20,25 +20,21 @@ public class Conversions {
     public static String convertDecimal(int number, int base){
         //char[] result = new char[100];
         String result = "";
+
         while (number > 0) {
             if (base == 16) {
-                int hexalNumber = number % 16;
+                int hexalNumber = number % base;
 
                 char hexDigit = (hexalNumber <= 9 && hexalNumber > 0) ? (char) (hexalNumber + '0')
                         : (char) (hexalNumber - 10 + 'A');
                 result = hexDigit + result;
-                number = number / 16;
+                number = number / base;
             }
-            if (base == 8) {
-                int remainder = number % 8;
+            if (base == 8 || base == 2) {
+                int remainder = number % base;
                 result = remainder + result;
 
-                number = number / 8;
-            }
-            if (base == 2) {
-                int remainder = number % 2;
-                result = remainder + result;
-                number = number / 2;
+                number = number / base;
             }
 
         }
--- src/task1/Main.java
+++ src/task1/Main.java
@@ -3,6 +3,6 @@ package task1;
 public class Main {
     public static void main(String [] args){
 
-        Check.checkConversion(3, 3);
+        Check.checkConversion(11, 16);
     }
 }
